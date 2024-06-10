4d21b5e388119393812fa4c9b0437eed6ab6a240
kyeong-hyeok <minkh4227@naver.com>
2024-06-10 15:36:56 +0900

    chore: 클래스명 변경(SameNumberCount -> Winning)

---
+++ src/main/java/Winning.java
@@ -0,0 +1,24 @@
+public class Winning {
+
+    int threeWinningCnt;
+    int fourWinningCnt;
+    int fiveWinningCnt;
+    int fiveBonusWinningCnt;
+    int sixWinningCnt;
+
+    public Winning(int threeWinningCnt, int fourWinningCnt, int fiveWinningCnt, int fiveBonusWinningCnt, int sixWinningCnt) {
+        this.threeWinningCnt = threeWinningCnt;
+        this.fourWinningCnt = fourWinningCnt;
+        this.fiveWinningCnt = fiveWinningCnt;
+        this.fiveBonusWinningCnt = fiveBonusWinningCnt;
+        this.sixWinningCnt = sixWinningCnt;
+    }
+
+    public int receiveWinningMoney() {
+        return WinningPrice.THREE.getPrice() * threeWinningCnt
+                + WinningPrice.FOUR.getPrice() * fourWinningCnt
+                + WinningPrice.FIVE.getPrice() * fiveWinningCnt
+                + WinningPrice.FIVE_BONUS.getPrice() * fiveBonusWinningCnt
+                + WinningPrice.SIX.getPrice() * sixWinningCnt;
+    }
+}
--- src/test/java/DrawTest.java
+++ src/test/java/DrawTest.java
@@ -52,40 +52,46 @@ class DrawTest {
     @DisplayName("3개 일치할 경우 장당 5000원을 받는다")
     @Test
     void three_numbers_same_get_money() {
-        SameNumberCount sameNumberCount = new SameNumberCount(1, 0, 0, 0, 0);
-        int money = sameNumberCount.receiveMoney();
+        Winning winning = new Winning(1, 0, 0, 0, 0);
+        int money = winning.receiveWinningMoney();
         assertThat(money).isEqualTo(5000);
     }
 
     @DisplayName("4개 일치할 경우 장당 50000원을 받는다")
     @Test
     void four_numbers_same_get_money() {
-        SameNumberCount sameNumberCount = new SameNumberCount(0, 1, 0, 0, 0);
-        int money = sameNumberCount.receiveMoney();
+        Winning winning = new Winning(0, 1, 0, 0, 0);
+        int money = winning.receiveWinningMoney();
         assertThat(money).isEqualTo(50000);
     }
 
     @DisplayName("5개 일치할 경우 장당 1500000원을 받는다.")
     @Test
     void five_numbers_same_get_money() {
-        SameNumberCount sameNumberCount = new SameNumberCount(0, 0, 1, 0, 0);
-        int money = sameNumberCount.receiveMoney();
+        Winning winning = new Winning(0, 0, 1, 0, 0);
+        int money = winning.receiveWinningMoney();
         assertThat(money).isEqualTo(1500000);
     }
 
     @DisplayName("5개 일치할 경우(보너스 볼 일치) 장당 30000000원을 받는다")
     @Test
     void five_bonus_numbers_same_get_money() {
-        SameNumberCount sameNumberCount = new SameNumberCount(0, 0, 0, 1, 0);
-        int money = sameNumberCount.receiveMoney();
+        Winning winning = new Winning(0, 0, 0, 1, 0);
+        int money = winning.receiveWinningMoney();
         assertThat(money).isEqualTo(30000000);
     }
 
     @DisplayName("6개 일치할 경우(보너스 볼 제외) 장당 2000000000원을 받는다")
     @Test
     void six_numbers_same_get_money() {
-        SameNumberCount sameNumberCount = new SameNumberCount(0, 0, 0, 0, 1);
-        int money = sameNumberCount.receiveMoney();
+        Winning winning = new Winning(0, 0, 0, 0, 1);
+        int money = winning.receiveWinningMoney();
         assertThat(money).isEqualTo(2000000000);
+    }
+
+    @DisplayName("수익률은 당첨 금액 / 구매한 금액을 소수점 두자리까지 구한다")
+    @Test
+    void total_return_rate() {
+
     }
 }
