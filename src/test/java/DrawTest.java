9f72fe9f65acc7c15358f7f81e51f354d511ad97
kyeong-hyeok <minkh4227@naver.com>
2024-06-10 15:25:09 +0900

    test: 로또 번호와 당첨 번호가 4개가 일치한다

--- src/test/java/DrawTest.java
+++ src/test/java/DrawTest.java
@@ -16,6 +16,14 @@ class DrawTest {
         assertEquals(lotto.getLottoNums().stream().filter(winningNumbers.getNumbers()::contains).count(), 3);
     }
 
+    @DisplayName("로또 번호와 당첨 번호가 4개가 일치한다")
+    @Test
+    void four_numbers_same() {
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1,2,3,4,8,9), 10);
+        assertEquals(lotto.getLottoNums().stream().filter(winningNumbers.getNumbers()::contains).count(), 4);
+    }
+
     @DisplayName("3개 일치할 경우 장당 5000원을 받는다")
     @Test
     void three_numbers_same_get_money() {
