8b9d459cbefa15cda178d1d4bb8a5c1f7a98c6ca
xoxolittlemonster4455@gmail.com <Ilovegaga0203!@>
2024-06-11 15:43:33 +0900

    fix: 로또 번호와 당첨번호 일치 여부 확인

--- src/main/java/Winning.java
+++ src/main/java/Winning.java
@@ -1,28 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Winning {
 
-    int threeWinningCnt;
-    int fourWinningCnt;
-    int fiveWinningCnt;
-    int fiveBonusWinningCnt;
-    int sixWinningCnt;
+    List<Integer> matchCnt;
+    final List<Lotto> lottoList;
+    private static final int LOTTO_RANK_SIZE = 5;
 
-    public Winning(int threeWinningCnt, int fourWinningCnt, int fiveWinningCnt, int fiveBonusWinningCnt, int sixWinningCnt) {
-        this.threeWinningCnt = threeWinningCnt;
-        this.fourWinningCnt = fourWinningCnt;
-        this.fiveWinningCnt = fiveWinningCnt;
-        this.fiveBonusWinningCnt = fiveBonusWinningCnt;
-        this.sixWinningCnt = sixWinningCnt;
+    public Winning(List<Lotto> lottoList) {
+        this.lottoList = lottoList;
     }
 
-    public int receiveWinningMoney() {
-        return WinningPrice.THREE.getPrice() * threeWinningCnt
-                + WinningPrice.FOUR.getPrice() * fourWinningCnt
-                + WinningPrice.FIVE.getPrice() * fiveWinningCnt
-                + WinningPrice.FIVE_BONUS.getPrice() * fiveBonusWinningCnt
-                + WinningPrice.SIX.getPrice() * sixWinningCnt;
-    }
 
-    public double calculateTotalRate(int cnt) {
-        return Math.round((double)receiveWinningMoney() / (cnt * 1000) * 100.0) / 100.0;
+    public List<Integer> getMatchCnt() {
+
+        List<Integer> matchCnt = new ArrayList<>(Collections.nCopies(LOTTO_RANK_SIZE + 1, 0));
+
+        lottoList.forEach(lotto -> {
+            int lottoRank = lotto.getLottoRank();
+            matchCnt.set(lottoRank, matchCnt.get(lottoRank) + 1);
+        });
+
+        return matchCnt;
     }
 }
+
+
+
+//    public int receiveWinningMoney() {
+//        return WinningPrice.THREE.getPrice() * threeWinningCnt
+//                + WinningPrice.FOUR.getPrice() * fourWinningCnt
+//                + WinningPrice.FIVE.getPrice() * fiveWinningCnt
+//                + WinningPrice.FIVE_BONUS.getPrice() * fiveBonusWinningCnt
+//                + WinningPrice.SIX.getPrice() * sixWinningCnt;
+//    }
+//
+//
+//
+//    public double calculateTotalRate(int cnt) {
+//        return Math.round((double)receiveWinningMoney() / (cnt * 1000) * 100.0) / 100.0;
+//    }
+//}
